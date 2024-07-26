0a2ea22c142a4d892a9db63e68a5824dd74c497a
Neelima <neelimaakkiraju2001@gmail.com>
2024-07-26 12:35:46 +0530

    first test

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import FlowComponent from './Components/FlowComponent'
+export default function App() {
+  return (
+    <div style={{width:'100%', height:'100vh'}}>
+      <FlowComponent />
+    </div>
+  )
+}
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  const onDragStart = (event, nodeType) => {
+    event.dataTransfer.setData('application/reactflow', nodeType);
+    event.dataTransfer.effectAllowed = 'move';
+  };
+  
+  return (
+    <aside>
+      <div className="description">Cloud Diagram</div>
+      <div className="dndnode input" onDragStart={(event) => onDragStart(event, 'input')} draggable>
+        Input Node
+      </div>
+      <div className="dndnode" onDragStart={(event) => onDragStart(event, 'default')} draggable>
+        Default Node
+      </div>
+      <div className="dndnode output" onDragStart={(event) => onDragStart(event, 'output')} draggable>
+        Output Node
+      </div>
+      
+    </aside>
+  );
+};
